51e62feef92146eb4ebafb8befb49ee205b137de
Tafitasoa <tafitasoa.cedric@gmail.com>
2018-04-12 16:18:34 +0300

    Commit 1.3

--- Route_de_la_mairie.rb
+++ Route_de_la_mairie.rb
@@ -21,11 +21,14 @@ end
 
 #   a3 - Bonnus : name => email
 Le_bonnus = []
-links = get_all_the_urls_of_val_doise_townhalls("http://annuaire-des-mairies.com/val-d-oise.html")
-links.each do |i|
-    Le_bonnus.push({
-        :name => Nokogiri::HTML(open(i)).css('main h1')[0].text.split(" ")[0],
-        :email => get_the_email_of_a_townhal_from_its_webpage(i)
-    })
-    puts Le_bonnus
+def show_name_email
+    links = get_all_the_urls_of_val_doise_townhalls("http://annuaire-des-mairies.com/val-d-oise.html")
+    links.each do |i|
+        Le_bonnus.push({
+            :name => Nokogiri::HTML(open(i)).css('main h1')[0].text.split(" ")[0],
+            :email => get_the_email_of_a_townhal_from_its_webpage(i)
+        })
+        puts Le_bonnus
+    end
 end
+show_name_email
