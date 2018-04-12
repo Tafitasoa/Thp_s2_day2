b5082c56eb34ba62cf3eecc36580d9804a9981e1
Tafitasoa <tafitasoa.cedric@gmail.com>
2018-04-12 15:56:48 +0300

    Commit 1.2

--- Route_de_la_mairie.rb
+++ Route_de_la_mairie.rb
@@ -1,23 +1,31 @@
 require 'open-uri'
 require 'nokogiri'
 
-
+# a - Route de la mairie
+#   a1 - Méthode qui récupère l'adresse email à partir de l'url d'une mairie, voici celle de Vauréal
 def get_the_email_of_a_townhal_from_its_webpage(page_url)
     doc = Nokogiri::HTML(open(page_url))
-    puts doc.css('.tr-last')[3].text.split(" ")[2]
+    doc.css('.tr-last')[3].text.split(" ")[2]
 end
 get_the_email_of_a_townhal_from_its_webpage("http://annuaire-des-mairies.com/95/vaureal.html")
 
+#   a2 - Méthode qui récupère toutes les url de villes du Val d'Oise.
+Url_tab = []
 def get_all_the_urls_of_val_doise_townhalls(page_url)
-    doc_2 = Nokogiri::HTML(open(page_url))
-
-    city = Hash.new(0)
-    
-    doc_2.css('.lientxt').each_with_object(city) do |town, mail|
-        town_name = town.text
-        link = "http://annuaire-des-mairies.com" + town['href'][1..-1]
-        email = get_the_email_of_a_townhal_from_its_webpage(link)
+    doc = Nokogiri::HTML(open(page_url))
+    doc.css('.lientxt').each do |link|
+        Url_tab.push("http://annuaire-des-mairies.com" + (link['href'][1..-1]))
     end
+    Url_tab
 end
-get_all_the_urls_of_val_doise_townhalls("http://annuaire-des-mairies.com/val-d-oise.html")
 
+#   a3 - Bonnus : name => email
+Le_bonnus = []
+links = get_all_the_urls_of_val_doise_townhalls("http://annuaire-des-mairies.com/val-d-oise.html")
+links.each do |i|
+    Le_bonnus.push({
+        :name => Nokogiri::HTML(open(i)).css('main h1')[0].text.split(" ")[0],
+        :email => get_the_email_of_a_townhal_from_its_webpage(i)
+    })
+    puts Le_bonnus
+end
